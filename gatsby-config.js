4c903bf7d6786a0a33e227eaf31d9f34867aed4d
Mori <irom.uw@gmail.com>
2025-06-09 20:56:32 +1000

    doc: update resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Mori Wu',
     description:
       'Mori Wu is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://moriwu.com', // No trailing slash allowed!
+    siteUrl: 'https://moriwu-portfolio.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
